97db6b9179ec909be9f1f956ac8768e985a7ba3f
Kelvin <klvnjntn@gmail.com>
2026-05-28 16:09:23 +0700

    hmm

--- modules/monitoring/grafana.tf
+++ modules/monitoring/grafana.tf
@@ -1,6 +1,11 @@
+resource "time_sleep" "wait_for_grafana" {
+  create_duration = "60s"  # Gives Grafana a full minute to complete database migrations
+}
+
 resource "grafana_data_source" "cloudwatch" {
   type = "cloudwatch"
   name = "AWS-CloudWatch"
+  depends_on = [time_sleep.wait_for_grafana]
 
   json_data_encoded = jsonencode({
     defaultRegion = "ap-southeast-1"
