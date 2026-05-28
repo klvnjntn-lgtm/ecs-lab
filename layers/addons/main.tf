1d72a0c619a9d11cbf2c99a6b3f060114187e433
Kelvin <klvnjntn@gmail.com>
2026-05-28 15:07:44 +0700

    addons

--- layers/addons/main.tf
+++ layers/addons/main.tf
@@ -1,10 +1,16 @@
 module "monitoring" {
-  source                  = "../../modules/monitoring"
-  project_name            = data.project_name
-  alb_arn_suffix          = data.alb_arn_suffix
-  target_group_arn_suffix = data.alb.tg_arn_suffix
-  target_group_2_arn_suffix = data.alb.tg_2_arn_suffix
-  alb_listener_rule_arn = data.alb.alb_listener_rule_monitoring_arn
-  alb_dns_name = data.alb.alb_dns_name
-  grafana_ready_signal = data.ecs.ecs_service_ready
+  source                    = "../../modules/monitoring"
+  
+  # Fix: Route all arguments to look inside data.terraform_remote_state.infra.outputs
+  project_name              = data.terraform_remote_state.infra.outputs.project_name
+  alb_arn_suffix            = data.terraform_remote_state.infra.outputs.alb_arn_suffix
+  target_group_arn_suffix   = data.terraform_remote_state.infra.outputs.tg_arn_suffix
+  target_group_2_arn_suffix = data.terraform_remote_state.infra.outputs.tg_2_arn_suffix
+  alb_listener_rule_arn     = data.terraform_remote_state.infra.outputs.alb_listener_rule_monitoring_arn
+  alb_dns_name              = data.terraform_remote_state.infra.outputs.alb_dns_name
+
+  # Since you deleted the grafana_ready check resource from the ECS layer, 
+  # pass a dummy value or a null string if the module variable accepts it, 
+  # or remove this line if you updated the monitoring module variables.
+  grafana_ready_signal      = "ready" 
 }
--- layers/addons/provider.tf
+++ layers/addons/provider.tf
@@ -6,3 +6,11 @@ data "terraform_remote_state" "infra" {
     region = "ap-southeast-1"
   }
 }
+
+provider "grafana" {
+  # Fix: Read from the remote state instead of module.alb
+  url  = "http://${data.terraform_remote_state.infra.outputs.alb_dns_name}/grafana/"
+  
+  # Fix: Read from the remote state instead of module.rds
+  auth = "dbadmin:${data.terraform_remote_state.infra.outputs.rds_password}"
+}
