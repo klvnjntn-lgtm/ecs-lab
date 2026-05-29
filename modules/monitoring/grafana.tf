4f7f3f0d52720997a0153a2da533d6a8f83d371b
Kelvin <klvnjntn@gmail.com>
2026-05-29 13:30:04 +0700

    grafanahealthcheck

--- modules/monitoring/grafana.tf
+++ modules/monitoring/grafana.tf
@@ -1,21 +1,18 @@
-resource "time_sleep" "grafana_boot_delay" {
-  create_duration = "90s"  
-}
+data "http" "grafana_healthcheck" {
+  url = "http://${data.terraform_remote_state.infra.outputs.alb_dns_name}/grafana/"
+    
+  retry {
+    attempts     = 20
+    min_delay_ms = 5000   # wait 5s between attempts
+    max_delay_ms = 10000
+  }
 
-resource "grafana_data_source" "cloudwatch" {
-  type = "cloudwatch"
-  name = "AWS-CloudWatch"
-  
-  # 🟢 FIX: Update the dependency to match the new timer name
-  depends_on = [
-    time_sleep.grafana_boot_delay,
-    var.grafana_ready_signal
-  ]
-
-  json_data_encoded = jsonencode({
-    defaultRegion = "ap-southeast-1"
-    authType      = "default"
-  })
+  lifecycle {
+    postcondition {
+      condition     = self.status_code == 200
+      error_message = "Grafana health check failed after retries"
+    }
+  }
 }
 
 resource "grafana_dashboard" "ecs_metrics" {
